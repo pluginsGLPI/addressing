b7cacd04c07793e84a13e67fb20d91c42186c93a
tsmr <tsmr>
2009-08-11 15:17:37 +0000

    Update sql for update to 0.80

--- inc/plugin_addressing-1.6-update.sql
+++ inc/plugin_addressing-1.6-update.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `glpi_plugin_addressing` 
+ALTER TABLE `glpi_plugin_addressing`
 	ADD `ipdeb` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL default '' AFTER `ipfin4`,
 	ADD `ipfin` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL default '' AFTER `ipdeb`;
 
@@ -6,7 +6,7 @@ UPDATE `glpi_plugin_addressing` SET
 	ipdeb=CONCAT_WS('.',ipdebut1,ipdebut2,ipdebut3,ipdebut4),
 	ipfin=CONCAT_WS('.',ipdebut1,ipdebut2,ipdebut3,ipfin4);
 
-ALTER TABLE `glpi_plugin_addressing` 
+ALTER TABLE `glpi_plugin_addressing`
 	ADD `ip_alloted` SMALLINT NOT NULL DEFAULT '1' AFTER `ipfin` ,
 	ADD `ip_double` SMALLINT NOT NULL DEFAULT '1' AFTER `ip_alloted` ,
 	ADD `ip_free` SMALLINT NOT NULL DEFAULT '1' AFTER `ip_double` ,
@@ -18,11 +18,11 @@ ALTER TABLE `glpi_plugin_addressing`
 	DROP `ipdebut3`,
 	DROP `ipdebut4`,
 	DROP `ipfin4`;
-	
-DELETE FROM `glpi_display` WHERE `type` = 5000;
-INSERT INTO `glpi_display` (`type` , `num` , `rank` , `FK_users` ) VALUES (5000,2,2,0);
-INSERT INTO `glpi_display` (`type` , `num` , `rank` , `FK_users` ) VALUES (5000,3,6,0);
-INSERT INTO `glpi_display` (`type` , `num` , `rank` , `FK_users` ) VALUES (5000,4,5,0);
-INSERT INTO `glpi_display` (`type` , `num` , `rank` , `FK_users` ) VALUES (5000,5,7,0);
-INSERT INTO `glpi_display` (`type` , `num` , `rank` , `FK_users` ) VALUES (5000,1000,3,0);
-INSERT INTO `glpi_display` (`type` , `num` , `rank` , `FK_users` ) VALUES (5000,1001,4,0);
+
+DELETE FROM `glpi_displayprefs` WHERE `type` = 5000;
+INSERT INTO `glpi_displayprefs` VALUES (5000,2,2,0);
+INSERT INTO `glpi_displayprefs` VALUES (5000,3,6,0);
+INSERT INTO `glpi_displayprefs` VALUES (5000,4,5,0);
+INSERT INTO `glpi_displayprefs` VALUES (5000,5,7,0);
+INSERT INTO `glpi_displayprefs` VALUES (5000,1000,3,0);
+INSERT INTO `glpi_displayprefs` VALUES (5000,1001,4,0);
