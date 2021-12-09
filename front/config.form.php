2de8dad11fb7887aa999965111b0971e40f54a32
tsmr <thetsmr@gmail.com>
2021-12-09 15:22:22 +0100

    Add file

---
+++ front/config.form.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * @version $Id$
+ -------------------------------------------------------------------------
+ addressing plugin for GLPI
+ Copyright (C) 2009-2016 by the addressing Development Team.
+
+ https://github.com/pluginsGLPI/addressing
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of addressing.
+
+ addressing is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ addressing is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with addressing. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
+
+include ('../../../inc/includes.php');
+
+$plugin = new Plugin();
+if ($plugin->isActivated("addressing")) {
+   $PluginAddressingConfig = new PluginAddressingConfig();
+
+   Session::checkRight("config", UPDATE);
+
+   if (isset($_POST["update"])) {
+      $PluginAddressingConfig->update($_POST);
+      Html::back();
+
+   } else {
+      Html::header(PluginAddressingAddressing::getTypeName(2), '', "tools", "pluginaddressingmenu", "addressing");
+      $PluginAddressingConfig->showForm(1);
+      Html::footer();
+   }
+
+} else {
+   Html::header(__('Setup'), '', "config", "plugins");
+   echo "<div class='alert alert-important alert-warning d-flex'>";
+   echo "<b>".__('Please activate the plugin', 'addressing')."</b></div>";
+   Html::footer();
+}
