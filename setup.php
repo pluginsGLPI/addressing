d9ecf4bf7c2f7ac59e1b3bd5c6cf577219d8d17a
tsmr <thetsmr@gmail.com>
2021-12-23 09:18:23 +0100

    Prepare rc2

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_ADDRESSING_VERSION', '3.0.0-rc1');
+define('PLUGIN_ADDRESSING_VERSION', '3.0.0-rc2');
 
 if (!defined("PLUGIN_ADDRESSING_DIR")) {
    define("PLUGIN_ADDRESSING_DIR", Plugin::getPhpDir("addressing"));
