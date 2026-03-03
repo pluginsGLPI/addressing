efcdcdc57134e6a4b21278813d0f36d7eb5ac4b9
Thetsmr <thetsmr@gmail.com>
2026-03-03 12:05:01 +0100

    Bump version 3.2.0

--- setup.php
+++ setup.php
@@ -35,7 +35,7 @@ use GlpiPlugin\Addressing\Profile;
 
 global $CFG_GLPI;
 
-define('PLUGIN_ADDRESSING_VERSION', '3.1.2');
+define('PLUGIN_ADDRESSING_VERSION', '3.2.0');
 
 if (!defined("PLUGIN_ADDRESSING_DIR")) {
     define("PLUGIN_ADDRESSING_DIR", Plugin::getPhpDir("addressing"));
